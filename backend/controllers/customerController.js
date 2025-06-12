fbad5d12bad48ba2a2b0f20883893f92cd37a7f6
Arshiya2005 <arshiyagkazi@gmail.com>
2025-06-13 01:25:06 +0530

    added code for profile -> myvehicles, info routes

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -1,4 +1,114 @@
+export const welcome = async (req, res) => {
+    try {
+        if(req.user.type === "customer") {
+            return res.status(200).json({ data: req.user });
+        }else {
+            return res.status(401).json({ error: "no active user" });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: "internal server error" });
+    }
+};
+
+export const myVehicles = async (req, res) => {
+    try {
+        if(req.user.type !== "customer") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const id = req.user.id;
+        const result = await sql`
+            SELECT * FROM vehicle WHERE customer_id = ${id}
+        `;
+        console.log(result);
+        return res.status(200).json({ data: result });
+    } catch (error) {
+        return res.status(500).json({ error: "internal server error" });
+    }
+};
+
+export const addVehicle = async (req, res) => {
+    try {
+        if(req.user.type !== "customer") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const name = req.body.name;
+        const no = req.body.no;
+        const type = req.body.type;
+        const id = req.user.id;
+
+        await sql`
+            INSERT INTO vehicle (model, type, number, customer_id) VALUES (${name}, ${type}, ${no}, ${id})
+        `;
+        return res.status(200).json({ message: "Vehicle added successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: "internal server error" });
+    }
+};
+
+
+export const deleteVehicle = async (req, res) => {
+    try {
+        if(req.user.type !== "customer") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const id = req.body.id;
+        const userId = req.user.id;
+
+        await sql`
+            DELETE FROM vehicle 
+            WHERE id = ${id} AND customer_id = ${userId}
+        `;
+        return res.status(200).json({ message: "Vehicle deleted successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: "internal server error" });
+    }
+};
+
+//info, editFname, editLname
+
 export const info = async (req, res) => {
-    console.log(req.user);
-    res.status(401).json({ user: req.user });
+    try {
+        if(req.user.type !== "customer") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        return res.status(200).json({ data: req.user });
+    } catch (error) {
+        return res.status(500).json({ error: "internal server error" });
+    }
+};
+
+export const editFname = async (req, res) => {
+    try {
+        if(req.user.type !== "customer") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const fname = req.body.fname;
+        const id = req.user.id;
+        await sql`
+            UPDATE customer
+            SET fname = ${fname}
+            WHERE id = ${id}
+        `;
+        return res.status(200).json({ message: "Updated successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: "internal server error" });
+    }
+};
+
+export const editLname = async (req, res) => {
+    try {
+        if(req.user.type !== "customer") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const lname = req.body.lname;
+        const id = req.user.id;
+        await sql`
+            UPDATE customer
+            SET fname = ${lname}
+            WHERE id = ${id}
+        `;
+        return res.status(200).json({ message: "Updated successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: "internal server error" });
+    }
 };
