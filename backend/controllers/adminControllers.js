447eba6a7af596f59b7d2a0878d028a1632edb21
Arshiya2005 <arshiyagkazi@gmail.com>
2025-06-25 20:29:32 +0530

    admin side code for owner data

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -18,7 +18,7 @@ export const welcome = async (req, res) => {
         `;
         const bookCount = book.length;
         const area = await sql`
-            SELECT * FROM parkingspot WHERE is_active = 'true';
+            SELECT * FROM parkingspot WHERE is_active = true;
         `;
         let bikeSlots = 0;
         let carSlots = 0;
@@ -44,7 +44,7 @@ export const ownerInfo = async (req, res) => {
         `;
         return res.status(200).json({ data : own });
     } catch (error) {
-        console.error("Error fetching counts in welcome:", error);
+        console.error("Error:", error);
         return res.status(500).json({ error: "Internal server error" });
     }
 };
@@ -56,11 +56,11 @@ export const ownerAreas = async (req, res) => {
         }
         const id = req.query.id;
         const area = await sql`
-            SELECT * FROM parkingspot WHERE is_active = 'true' AND owner_id = ${id};
+            SELECT * FROM parkingspot WHERE owner_id = ${id};
         `;
         return res.status(200).json({ data : area });
     } catch (error) {
-        console.error("Error fetching counts in welcome:", error);
+        console.error("Error:", error);
         return res.status(500).json({ error: "Internal server error" });
     }
 };
@@ -74,14 +74,14 @@ export const areaActiveBookings = async (req, res) => {
         const now = new Date();
         const today = now.toISOString().slice(0, 10);
         const response = await sql`
-            SELECT * FROM bookings WHERE slot_id = ${id} AND date = ${today} AND status = 'active' ORDER BY sTime ASC
+            SELECT * FROM bookings WHERE slot_id = ${id} AND date = ${today} AND status = 'active' ORDER BY sTime DESC
         `;
         if (response.length > 0) {
             return res.status(200).json({ data : response});
         }
         return res.status(200).json({ message: "No bookings" });
     } catch (error) {
-        console.error("Error fetching counts in welcome:", error);
+        console.error("Error:", error);
         return res.status(500).json({ error: "Internal server error" });
     }
 };
@@ -93,14 +93,87 @@ export const areaBookingHistory = async (req, res) => {
         }
         const id = req.query.id;
         const response = await sql`
-            SELECT * FROM bookings WHERE slot_id = ${id} ORDER BY sTime ASC
+            SELECT * FROM bookings WHERE slot_id = ${id} ORDER BY date DESC, sTime DESC
         `;
         if (response.length > 0) {
             return res.status(200).json({ data : response});
         }
         return res.status(200).json({ message: "No bookings" });
     } catch (error) {
-        console.error("Error fetching counts in welcome:", error);
+        console.error("Error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const customerInfo = async (req, res) => {
+    try {
+        if(req.user.type !== "admin") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const cust = await sql`
+            SELECT * FROM users WHERE type = ${"customer"};
+        `;
+        return res.status(200).json({ data : cust });
+    } catch (error) {
+        console.error("Error :", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+export const customerVehicles = async (req, res) => {
+    try {
+        if(req.user.type !== "admin") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const id = req.query.id;
+        const vehicles = await sql`
+            SELECT * FROM vehicle WHERE customer_id = ${id};
+        `;
+        return res.status(200).json({ data : vehicles });
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+export const customerActiveBooking = async (req, res) => {
+    try {
+        if(req.user.type !== "admin") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const id = req.query.id;
+        const now = new Date();
+        const today = now.toISOString().slice(0, 10);
+        const response = await sql`
+            SELECT * FROM bookings WHERE customer_id = ${id} AND date = ${today} AND status = 'active' ORDER BY sTime DESC
+        `;
+        if (response.length > 0) {
+            return res.status(200).json({ data : response});
+        }
+        return res.status(200).json({ message: "No bookings" });
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const customerBookingHistory = async (req, res) => {
+    try {
+        if(req.user.type !== "admin") {
+            return res.status(401).json({ error: "no active user" });
+        }
+        const id = req.query.id;
+        const response = await sql`
+            SELECT * FROM bookings WHERE customer_id = ${id} ORDER BY date DESC, sTime DESC
+        `;
+        if (response.length > 0) {
+            return res.status(200).json({ data : response});
+        }
+        return res.status(200).json({ message: "No bookings" });
+    } catch (error) {
+        console.error("Error:", error);
         return res.status(500).json({ error: "Internal server error" });
     }
 };
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,7 +2,8 @@ import express from "express";
 
 import {isAuthenticated} from "../controllers/authController.js"
 
-import { welcome , ownerInfo , ownerAreas , areaActiveBookings , areaBookingHistory } from "../controllers/adminControllers.js"
+import { welcome , ownerInfo , ownerAreas , areaActiveBookings , areaBookingHistory , customerActiveBooking, customerBookingHistory} from "../controllers/adminControllers.js"
+import { customerInfo, customerVehicles } from "../controllers/adminControllers.js"
 
 const router = express.Router();
 
@@ -11,5 +12,9 @@ router.get("/ownerInfo", isAuthenticated, ownerInfo);
 router.get("/ownerAreas", isAuthenticated, ownerAreas);
 router.get("/areaActiveBookings", isAuthenticated, areaActiveBookings);
 router.get("/areaBookingHistory", isAuthenticated, areaBookingHistory);
+router.get("/customerInfo", isAuthenticated, customerInfo);
+router.get("/customerVehicles", isAuthenticated, customerVehicles);
+router.get("/customerActiveBooking", isAuthenticated, customerActiveBooking);
+router.get("/customerBookingHistory", isAuthenticated, customerBookingHistory);
 
 export default router;
