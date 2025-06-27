3e020a046c7b9cb5d307c3da13a0e1213d7de8f4
Arshiya2005 <arshiyagkazi@gmail.com>
2025-06-27 14:35:11 +0530

    checking if not null in payment sheduler

--- backend/shedulers/payout.js
+++ backend/shedulers/payout.js
@@ -22,36 +22,37 @@ cron.schedule('* * * * *', async () => {
       const p = response[0];
       console.log("pending payout : ");
       console.log(p);
-      const amount = p.amount;
+      if(p) {
+        const amount = p.amount;
       const fund_account_id = p.fund_account_id;
       try {
-              await axios.post('https://api.razorpay.com/v1/payouts',
-                  {
-                      account_number,
-                      fund_account_id,
-                      amount,
-                      currency: "INR",
-                      mode: "IMPS",
-                      purpose: "payout",
-                      reference_id: `payout_${p.id}`,
-                      queue_if_low_balance: true,
-                      narration: "SmartParking: Slot Rent"
-                  },
-                  {
-                      auth: {
-                      username: process.env.KEY_ID,
-                      password: process.env.KEY_SECRET
-                      },
-                      headers: {
-                      'Content-Type': 'application/json',
-                      'X-Payout-Idempotency': crypto.randomUUID()
-                      }
-                  }
-              );
-              await sql`
-                  UPDATE pending_payouts SET status = 'completed', processed_at = ${new Date()}
-                  WHERE id = ${p.id};
-              `;
+            await axios.post('https://api.razorpay.com/v1/payouts',
+                {
+                    account_number,
+                    fund_account_id,
+                    amount,
+                    currency: "INR",
+                    mode: "IMPS",
+                    purpose: "payout",
+                    reference_id: `payout_${p.id}`,
+                    queue_if_low_balance: true,
+                    narration: "SmartParking: Slot Rent"
+                },
+                {
+                    auth: {
+                    username: process.env.KEY_ID,
+                    password: process.env.KEY_SECRET
+                    },
+                    headers: {
+                    'Content-Type': 'application/json',
+                    'X-Payout-Idempotency': crypto.randomUUID()
+                    }
+                }
+            );
+            await sql`
+                UPDATE pending_payouts SET status = 'completed', processed_at = ${new Date()}
+                WHERE id = ${p.id};
+            `;
           }catch(error) {
               console.log(p.id + " : payout failed");
               await sql`
@@ -59,6 +60,7 @@ cron.schedule('* * * * *', async () => {
                   WHERE id = ${p.id};
               `;
           }
+      }
     }
   } catch (error) {
     console.error("Razorpay Payout Error:", error.response?.data || error.message);
